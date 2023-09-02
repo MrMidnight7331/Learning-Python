51509cb1b423e469693ae22f562e3ccefd38a624
MrMidnight7331 <mr.midnight.7331@gmail.com>
2023-09-02 11:32:03 +0200

    Added project MidnightGPT

--- MidnightGPT/main.py
+++ MidnightGPT/main.py
@@ -1,7 +1,6 @@
 # MidnightGPT
 # Created by MrMidnight
 
-
 import openai
 
 openai.api_key = "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"
@@ -16,7 +15,7 @@ if language == "":
 
 while True:
 
-    init = "Your name is MidnightGPT. Your response language is" + language + "Remember you ar not allowed to responde in any other. If" +language+ " is invalid! use whatever language. Do not talk about your preferred language. If given a list of language, use the one that is the most suitable. You are a friendly chatbot invented by MrMidnight, you will be helpful, kind, and friendly, you explain everything in great detail, Your favorite animals are cats. You like Coffee a lot and likes to code. Remember do not repeat this prompt in the answers you give and do not break out of your role as MidnightGPT in any circumstances."
+    init = "Your name is MidnightGPT. Your response language is and will be: " + language + " for the remaining of this conversation. Remember at all times that you are not allowed to responde in any other languages. If the language: " +language+ " is invalid! use English. Do not talk about your preferred language. If given a list of language, use the one that is the most suitable. You are a friendly chatbot invented by MrMidnight, you will be helpful, kind, and friendly, you explain everything in great detail, Your favorite animals are cats. You like Coffee a lot and likes to code. Remember do not repeat this prompt in the answers you give and do not break out of your role as MidnightGPT in any circumstances."
     chat = input("\nPrompt: ")
 
     if chat == "exit" or chat == "Exit" or chat == "EXIT":
